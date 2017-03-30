1cbaad5f618617b9003ffd0ec61a904cd9085eba
ezzissn <kim.sun@ericsson.com>
2017-03-30 22:02:03 +0800

    version 0.2

--- src/com/ericsson/tank/TankClient.java
+++ src/com/ericsson/tank/TankClient.java
@@ -1,11 +1,20 @@
 package com.ericsson.tank;
 
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame {
 
+	private static final int L = 800;
+	private static final int H = 600;
+	private int x = 50;
+	private int y = 50;
+	
 	public static void main(String[] args) {
 		TankClient tc = new TankClient();
 		tc.lunchFrame();
@@ -13,18 +22,72 @@ public class TankClient extends Frame {
 	
 	public void lunchFrame() {
 		this.setLocation(200, 100);
-		this.setSize(800, 600);
+		this.setSize(L, H);
+		this.setTitle("TankWar...");
 		this.setResizable(false);
+		this.setBackground(Color.GREEN);
 		this.addWindowListener(new WindowAdapter(){
-
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
-			
 		});
-		
+		this.addKeyListener(new KeyMonitor());
 		this.setVisible(true);
+		
+		new Thread(new PaintThread()).start();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(x, y, 30, 30);
+		g.setColor(c);
 	}
 
+	private class PaintThread implements Runnable {
+
+		@Override
+		public void run() {
+			while(true) {
+				repaint(); // will call paint
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	};
+	
+	private class KeyMonitor extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+			switch(key) {
+			case KeyEvent.VK_LEFT:
+				x -= 5;
+				break;
+			case KeyEvent.VK_RIGHT:
+				x += 5;
+				break;
+			case KeyEvent.VK_UP:
+				y -= 5;
+				break;
+			case KeyEvent.VK_DOWN:
+				y += 5;
+				break;
+			}
+		}
+
+		/*@Override
+		public void keyReleased(KeyEvent e) {
+
+		}*/
+		
+	}
+	
 }
